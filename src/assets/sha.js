3c47f32bfab2ccb7b54b51b9ef8f328e4b872d76
jainrock <jainrocks999@gmail.com>
2020-09-26 17:42:54 +0530

    26092020YY

---
+++ src/assets/sha.js
@@ -0,0 +1,275 @@
+// > Task :app:signingReport
+// Variant: releaseUnitTest
+// Config: release
+// Store: G:\Office\2Sep\TTMApp\android\app\ttm_app.keystore
+// Alias: my-key-alias
+// MD5: DC:33:D3:D9:27:8B:52:9A:44:F6:F6:2B:F5:17:8F:9D
+// SHA1: 12:08:B1:25:B0:E0:07:07:05:2C:70:3B:AC:48:ED:8D:45:88:7C:7E
+// SHA-256: 80:13:6B:56:B3:F2:00:C4:67:FF:9A:63:E3:95:8B:89:8A:DD:37:48:E5:C6:C0:0C:5A:05:74:2E:9D:F7:B3:3B
+// Valid until: Sunday, December 29, 2047
+// ----------
+// Variant: release
+// Config: release
+// Store: G:\Office\2Sep\TTMApp\android\app\ttm_app.keystore
+// Alias: my-key-alias
+// MD5: DC:33:D3:D9:27:8B:52:9A:44:F6:F6:2B:F5:17:8F:9D
+// SHA1: 12:08:B1:25:B0:E0:07:07:05:2C:70:3B:AC:48:ED:8D:45:88:7C:7E
+// SHA-256: 80:13:6B:56:B3:F2:00:C4:67:FF:9A:63:E3:95:8B:89:8A:DD:37:48:E5:C6:C0:0C:5A:05:74:2E:9D:F7:B3:3B
+// Valid until: Sunday, December 29, 2047
+// ----------
+// Variant: debugUnitTest
+// Config: debug
+// Store: G:\Office\2Sep\TTMApp\android\app\debug.keystore
+// Alias: androiddebugkey
+// MD5: 20:F4:61:48:B7:2D:8E:5E:5C:A2:3D:37:A4:F4:14:90
+// SHA1: 5E:8F:16:06:2E:A3:CD:2C:4A:0D:54:78:76:BA:A6:F3:8C:AB:F6:25
+// SHA-256: FA:C6:17:45:DC:09:03:78:6F:B9:ED:E6:2A:96:2B:39:9F:73:48:F0:BB:6F:89:9B:83:32:66:75:91:03:3B:9C
+// Valid until: Wednesday, May 1, 2052
+// ----------
+// Variant: debugAndroidTest
+// Config: debug
+// Store: G:\Office\2Sep\TTMApp\android\app\debug.keystore
+// Alias: androiddebugkey
+// MD5: 20:F4:61:48:B7:2D:8E:5E:5C:A2:3D:37:A4:F4:14:90
+// SHA1: 5E:8F:16:06:2E:A3:CD:2C:4A:0D:54:78:76:BA:A6:F3:8C:AB:F6:25
+// SHA-256: FA:C6:17:45:DC:09:03:78:6F:B9:ED:E6:2A:96:2B:39:9F:73:48:F0:BB:6F:89:9B:83:32:66:75:91:03:3B:9C
+// Valid until: Wednesday, May 1, 2052
+// ----------
+// Variant: debug
+// Config: debug
+// Store: G:\Office\2Sep\TTMApp\android\app\debug.keystore
+// Alias: androiddebugkey
+// MD5: 20:F4:61:48:B7:2D:8E:5E:5C:A2:3D:37:A4:F4:14:90
+// SHA1: 5E:8F:16:06:2E:A3:CD:2C:4A:0D:54:78:76:BA:A6:F3:8C:AB:F6:25
+// SHA-256: FA:C6:17:45:DC:09:03:78:6F:B9:ED:E6:2A:96:2B:39:9F:73:48:F0:BB:6F:89:9B:83:32:66:75:91:03:3B:9C
+// Valid until: Wednesday, May 1, 2052
+// ----------
+
+// > Task :react-native-cardview:signingReport
+// Variant: debugAndroidTest
+// Config: debug
+// Store: C:\Users\Yogendra Yadav\.android\debug.keystore
+// Alias: AndroidDebugKey
+// MD5: 1B:36:3C:05:C8:41:68:4E:BD:4F:A5:E8:8C:D3:2D:74
+// SHA1: F4:4F:06:ED:9B:BA:66:76:C3:EB:CA:E5:7D:34:72:32:A4:62:4C:F2
+// SHA-256: 0F:CD:F5:E4:C7:0E:B7:1A:FF:55:A7:8E:CB:7C:3A:6A:56:96:36:D6:70:09:70:45:28:16:E1:50:DC:F9:19:DA
+// Valid until: Saturday, June 25, 2050
+// ----------
+// Variant: debugUnitTest
+// Config: debug
+// Store: C:\Users\Yogendra Yadav\.android\debug.keystore
+// Alias: AndroidDebugKey
+// MD5: 1B:36:3C:05:C8:41:68:4E:BD:4F:A5:E8:8C:D3:2D:74
+// SHA1: F4:4F:06:ED:9B:BA:66:76:C3:EB:CA:E5:7D:34:72:32:A4:62:4C:F2
+// SHA-256: 0F:CD:F5:E4:C7:0E:B7:1A:FF:55:A7:8E:CB:7C:3A:6A:56:96:36:D6:70:09:70:45:28:16:E1:50:DC:F9:19:DA
+// Valid until: Saturday, June 25, 2050
+// ----------
+// Variant: releaseUnitTest
+// Config: none
+// ----------
+
+// > Task :react-native-community_async-storage:signingReport
+// Variant: releaseUnitTest
+// Config: none
+// ----------
+// Variant: debugUnitTest
+// Config: debug
+// Store: C:\Users\Yogendra Yadav\.android\debug.keystore
+// Alias: AndroidDebugKey
+// MD5: 1B:36:3C:05:C8:41:68:4E:BD:4F:A5:E8:8C:D3:2D:74
+// SHA1: F4:4F:06:ED:9B:BA:66:76:C3:EB:CA:E5:7D:34:72:32:A4:62:4C:F2
+// SHA-256: 0F:CD:F5:E4:C7:0E:B7:1A:FF:55:A7:8E:CB:7C:3A:6A:56:96:36:D6:70:09:70:45:28:16:E1:50:DC:F9:19:DA
+// Valid until: Saturday, June 25, 2050
+// ----------
+// Variant: debugAndroidTest
+// Config: debug
+// Store: C:\Users\Yogendra Yadav\.android\debug.keystore
+// Alias: AndroidDebugKey
+// MD5: 1B:36:3C:05:C8:41:68:4E:BD:4F:A5:E8:8C:D3:2D:74
+// SHA1: F4:4F:06:ED:9B:BA:66:76:C3:EB:CA:E5:7D:34:72:32:A4:62:4C:F2
+// SHA-256: 0F:CD:F5:E4:C7:0E:B7:1A:FF:55:A7:8E:CB:7C:3A:6A:56:96:36:D6:70:09:70:45:28:16:E1:50:DC:F9:19:DA
+// Valid until: Saturday, June 25, 2050
+// ----------
+
+// > Task :react-native-community_checkbox:signingReport
+// Variant: releaseUnitTest
+// Config: none
+// ----------
+// Variant: debugUnitTest
+// Config: debug
+// Store: C:\Users\Yogendra Yadav\.android\debug.keystore
+// Alias: AndroidDebugKey
+// MD5: 1B:36:3C:05:C8:41:68:4E:BD:4F:A5:E8:8C:D3:2D:74
+// SHA1: F4:4F:06:ED:9B:BA:66:76:C3:EB:CA:E5:7D:34:72:32:A4:62:4C:F2
+// SHA-256: 0F:CD:F5:E4:C7:0E:B7:1A:FF:55:A7:8E:CB:7C:3A:6A:56:96:36:D6:70:09:70:45:28:16:E1:50:DC:F9:19:DA
+// Valid until: Saturday, June 25, 2050
+// ----------
+// Variant: debugAndroidTest
+// Config: debug
+// Store: C:\Users\Yogendra Yadav\.android\debug.keystore
+// Alias: AndroidDebugKey
+// MD5: 1B:36:3C:05:C8:41:68:4E:BD:4F:A5:E8:8C:D3:2D:74
+// SHA1: F4:4F:06:ED:9B:BA:66:76:C3:EB:CA:E5:7D:34:72:32:A4:62:4C:F2
+// SHA-256: 0F:CD:F5:E4:C7:0E:B7:1A:FF:55:A7:8E:CB:7C:3A:6A:56:96:36:D6:70:09:70:45:28:16:E1:50:DC:F9:19:DA
+// Valid until: Saturday, June 25, 2050
+// ----------
+
+// > Task :react-native-community_masked-view:signingReport
+// Variant: releaseUnitTest
+// Config: none
+// ----------
+// Variant: debugAndroidTest
+// Config: debug
+// Store: C:\Users\Yogendra Yadav\.android\debug.keystore
+// Alias: AndroidDebugKey
+// MD5: 1B:36:3C:05:C8:41:68:4E:BD:4F:A5:E8:8C:D3:2D:74
+// SHA1: F4:4F:06:ED:9B:BA:66:76:C3:EB:CA:E5:7D:34:72:32:A4:62:4C:F2
+// SHA-256: 0F:CD:F5:E4:C7:0E:B7:1A:FF:55:A7:8E:CB:7C:3A:6A:56:96:36:D6:70:09:70:45:28:16:E1:50:DC:F9:19:DA
+// Valid until: Saturday, June 25, 2050
+// ----------
+// Variant: debugUnitTest
+// Config: debug
+// Store: C:\Users\Yogendra Yadav\.android\debug.keystore
+// Alias: AndroidDebugKey
+// MD5: 1B:36:3C:05:C8:41:68:4E:BD:4F:A5:E8:8C:D3:2D:74
+// SHA1: F4:4F:06:ED:9B:BA:66:76:C3:EB:CA:E5:7D:34:72:32:A4:62:4C:F2
+// SHA-256: 0F:CD:F5:E4:C7:0E:B7:1A:FF:55:A7:8E:CB:7C:3A:6A:56:96:36:D6:70:09:70:45:28:16:E1:50:DC:F9:19:DA
+// Valid until: Saturday, June 25, 2050
+// ----------
+
+// > Task :react-native-gesture-handler:signingReport
+// Variant: releaseUnitTest
+// Config: none
+// ----------
+// Variant: debugAndroidTest
+// Config: debug
+// Store: C:\Users\Yogendra Yadav\.android\debug.keystore
+// Alias: AndroidDebugKey
+// MD5: 1B:36:3C:05:C8:41:68:4E:BD:4F:A5:E8:8C:D3:2D:74
+// SHA1: F4:4F:06:ED:9B:BA:66:76:C3:EB:CA:E5:7D:34:72:32:A4:62:4C:F2
+// SHA-256: 0F:CD:F5:E4:C7:0E:B7:1A:FF:55:A7:8E:CB:7C:3A:6A:56:96:36:D6:70:09:70:45:28:16:E1:50:DC:F9:19:DA
+// Valid until: Saturday, June 25, 2050
+// ----------
+// Variant: debugUnitTest
+// Config: debug
+// Store: C:\Users\Yogendra Yadav\.android\debug.keystore
+// Alias: AndroidDebugKey
+// MD5: 1B:36:3C:05:C8:41:68:4E:BD:4F:A5:E8:8C:D3:2D:74
+// SHA1: F4:4F:06:ED:9B:BA:66:76:C3:EB:CA:E5:7D:34:72:32:A4:62:4C:F2
+// SHA-256: 0F:CD:F5:E4:C7:0E:B7:1A:FF:55:A7:8E:CB:7C:3A:6A:56:96:36:D6:70:09:70:45:28:16:E1:50:DC:F9:19:DA
+// Valid until: Saturday, June 25, 2050
+// ----------
+
+// > Task :react-native-linear-gradient:signingReport
+// Variant: debugUnitTest
+// Config: debug
+// Store: C:\Users\Yogendra Yadav\.android\debug.keystore
+// Alias: AndroidDebugKey
+// MD5: 1B:36:3C:05:C8:41:68:4E:BD:4F:A5:E8:8C:D3:2D:74
+// SHA1: F4:4F:06:ED:9B:BA:66:76:C3:EB:CA:E5:7D:34:72:32:A4:62:4C:F2
+// SHA-256: 0F:CD:F5:E4:C7:0E:B7:1A:FF:55:A7:8E:CB:7C:3A:6A:56:96:36:D6:70:09:70:45:28:16:E1:50:DC:F9:19:DA
+// Valid until: Saturday, June 25, 2050
+// ----------
+// Variant: releaseUnitTest
+// Config: none
+// ----------
+// Variant: debugAndroidTest
+// Config: debug
+// Store: C:\Users\Yogendra Yadav\.android\debug.keystore
+// Alias: AndroidDebugKey
+// MD5: 1B:36:3C:05:C8:41:68:4E:BD:4F:A5:E8:8C:D3:2D:74
+// SHA1: F4:4F:06:ED:9B:BA:66:76:C3:EB:CA:E5:7D:34:72:32:A4:62:4C:F2
+// SHA-256: 0F:CD:F5:E4:C7:0E:B7:1A:FF:55:A7:8E:CB:7C:3A:6A:56:96:36:D6:70:09:70:45:28:16:E1:50:DC:F9:19:DA
+// Valid until: Saturday, June 25, 2050
+// ----------
+
+// > Task :react-native-reanimated:signingReport
+// Variant: releaseUnitTest
+// Config: none
+// ----------
+// Variant: debugUnitTest
+// Config: debug
+// Store: C:\Users\Yogendra Yadav\.android\debug.keystore
+// Alias: AndroidDebugKey
+// MD5: 1B:36:3C:05:C8:41:68:4E:BD:4F:A5:E8:8C:D3:2D:74
+// SHA1: F4:4F:06:ED:9B:BA:66:76:C3:EB:CA:E5:7D:34:72:32:A4:62:4C:F2
+// SHA-256: 0F:CD:F5:E4:C7:0E:B7:1A:FF:55:A7:8E:CB:7C:3A:6A:56:96:36:D6:70:09:70:45:28:16:E1:50:DC:F9:19:DA
+// Valid until: Saturday, June 25, 2050
+// ----------
+// Variant: debugAndroidTest
+// Config: debug
+// Store: C:\Users\Yogendra Yadav\.android\debug.keystore
+// Alias: AndroidDebugKey
+// MD5: 1B:36:3C:05:C8:41:68:4E:BD:4F:A5:E8:8C:D3:2D:74
+// SHA1: F4:4F:06:ED:9B:BA:66:76:C3:EB:CA:E5:7D:34:72:32:A4:62:4C:F2
+// SHA-256: 0F:CD:F5:E4:C7:0E:B7:1A:FF:55:A7:8E:CB:7C:3A:6A:56:96:36:D6:70:09:70:45:28:16:E1:50:DC:F9:19:DA
+// Valid until: Saturday, June 25, 2050
+// ----------
+
+// > Task :react-native-safe-area-context:signingReport
+// Variant: debugAndroidTest
+// Config: debug
+// Store: C:\Users\Yogendra Yadav\.android\debug.keystore
+// Alias: AndroidDebugKey
+// MD5: 1B:36:3C:05:C8:41:68:4E:BD:4F:A5:E8:8C:D3:2D:74
+// SHA1: F4:4F:06:ED:9B:BA:66:76:C3:EB:CA:E5:7D:34:72:32:A4:62:4C:F2
+// SHA-256: 0F:CD:F5:E4:C7:0E:B7:1A:FF:55:A7:8E:CB:7C:3A:6A:56:96:36:D6:70:09:70:45:28:16:E1:50:DC:F9:19:DA
+// Valid until: Saturday, June 25, 2050
+// ----------
+// Variant: releaseUnitTest
+// Config: none
+// ----------
+// Variant: debugUnitTest
+// Config: debug
+// Store: C:\Users\Yogendra Yadav\.android\debug.keystore
+// Alias: AndroidDebugKey
+// MD5: 1B:36:3C:05:C8:41:68:4E:BD:4F:A5:E8:8C:D3:2D:74
+// SHA1: F4:4F:06:ED:9B:BA:66:76:C3:EB:CA:E5:7D:34:72:32:A4:62:4C:F2
+// SHA-256: 0F:CD:F5:E4:C7:0E:B7:1A:FF:55:A7:8E:CB:7C:3A:6A:56:96:36:D6:70:09:70:45:28:16:E1:50:DC:F9:19:DA
+// Valid until: Saturday, June 25, 2050
+// ----------
+
+// > Task :react-native-screens:signingReport
+// Variant: releaseUnitTest
+// Config: none
+// ----------
+// Variant: debugUnitTest
+// Config: debug
+// Store: C:\Users\Yogendra Yadav\.android\debug.keystore
+// Alias: AndroidDebugKey
+// MD5: 1B:36:3C:05:C8:41:68:4E:BD:4F:A5:E8:8C:D3:2D:74
+// SHA1: F4:4F:06:ED:9B:BA:66:76:C3:EB:CA:E5:7D:34:72:32:A4:62:4C:F2
+// SHA-256: 0F:CD:F5:E4:C7:0E:B7:1A:FF:55:A7:8E:CB:7C:3A:6A:56:96:36:D6:70:09:70:45:28:16:E1:50:DC:F9:19:DA
+// Valid until: Saturday, June 25, 2050
+// ----------
+// Variant: debugAndroidTest
+// Config: debug
+// Store: C:\Users\Yogendra Yadav\.android\debug.keystore
+// Alias: AndroidDebugKey
+// MD5: 1B:36:3C:05:C8:41:68:4E:BD:4F:A5:E8:8C:D3:2D:74
+// SHA1: F4:4F:06:ED:9B:BA:66:76:C3:EB:CA:E5:7D:34:72:32:A4:62:4C:F2
+// SHA-256: 0F:CD:F5:E4:C7:0E:B7:1A:FF:55:A7:8E:CB:7C:3A:6A:56:96:36:D6:70:09:70:45:28:16:E1:50:DC:F9:19:DA
+// Valid until: Saturday, June 25, 2050
+// ----------
+
+// > Task :react-native-vector-icons:signingReport
+// Variant: debugUnitTest
+// Config: debug
+// Store: C:\Users\Yogendra Yadav\.android\debug.keystore
+// Alias: AndroidDebugKey
+// MD5: 1B:36:3C:05:C8:41:68:4E:BD:4F:A5:E8:8C:D3:2D:74
+// SHA1: F4:4F:06:ED:9B:BA:66:76:C3:EB:CA:E5:7D:34:72:32:A4:62:4C:F2
+// SHA-256: 0F:CD:F5:E4:C7:0E:B7:1A:FF:55:A7:8E:CB:7C:3A:6A:56:96:36:D6:70:09:70:45:28:16:E1:50:DC:F9:19:DA
+// Valid until: Saturday, June 25, 2050
+// ----------
+// Variant: releaseUnitTest
+// Config: none
+// ----------
+// Variant: debugAndroidTest
+// Config: debug
+// Store: C:\Users\Yogendra Yadav\.android\debug.keystore
+// Alias: AndroidDebugKey
+// MD5: 1B:36:3C:05:C8:41:68:4E:BD:4F:A5:E8:8C:D3:2D:74
+// SHA1: F4:4F:06:ED:9B:BA:66:76:C3:EB:CA:E5:7D:34:72:32:A4:62:4C:F2
+// SHA-256: 0F:CD:F5:E4:C7:0E:B7:1A:FF:55:A7:8E:CB:7C:3A:6A:56:96:36:D6:70:09:70:45:28:16:E1:50:DC:F9:19:DA
+// Valid until: Saturday, June 25, 2050
